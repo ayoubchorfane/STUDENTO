df5e86a10953dadf9fa66b6436c4208fc9028792
Ayoub Chorfane <ayoubchorfane3@gmail.com>
2026-02-01 14:08:14 +0100

    Add Student class

---
+++ src/Student.java
@@ -0,0 +1,37 @@
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private double grade;
+
+    public Student(int id, String name, int age, double grade) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public double getGrade() {
+        return grade;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Age: " + age + ", Grade: " + grade;
+    }
+}
